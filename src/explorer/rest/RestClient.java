06917f94058bad956a155bea0a4d678fb2d942b3
Yay Online Yayment <31856212+yayOnlineYayment@users.noreply.github.com>
2018-11-13 10:32:56 -0500

    rest client

--- src/explorer/rest/RestClient.java
+++ src/explorer/rest/RestClient.java
@@ -1,5 +1,16 @@
 package explorer.rest;
 
+import java.io.*;
+import java.net.*;
+import java.util.stream.Collectors;
+
 public class RestClient
 {
+   public static String read(String urlString) throws IOException
+   {
+      URL url = new URL(urlString);
+      URLConnection connection = url.openConnection();
+      return new BufferedReader(new InputStreamReader(
+              connection.getInputStream())).lines().collect(Collectors.joining());
+   }
 }
